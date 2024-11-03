9e27f672bc96dc0471ad3e4c694aa9115f69a6cc
Gonzalo Vega UTN <129915204+Gonzalo-Vega-UTN@users.noreply.github.com>
2024-11-02 23:34:11 -0300

    Update EnvironmentVariablesConfig.java

--- src/main/java/com/example/buensaborback/configs/EnvironmentVariablesConfig.java
+++ src/main/java/com/example/buensaborback/configs/EnvironmentVariablesConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Profile({"local", "dev"})
 public class EnvironmentVariablesConfig {
 
     private final Dotenv dotenv;
